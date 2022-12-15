50f606d7fd58f1a1f7d48baedec5cd747f448deb
seanf316 <seanfinn316@gmail.com>
2022-12-15 00:20:56 +0000

    Updated map.js

---
+++ map.js
@@ -0,0 +1,64 @@
+  
+/**
+ * To run this file in Gitpod, use the 
+ * command node map.js in the terminal
+ */
+
+// Using a for loop
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+for (let num of nums) {
+    results.push(num * 2)
+};
+
+console.log(results)
+
+// Using map()
+const multiplyByTwo = function (num) {
+    return num * 2;
+}
+
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults)
+
+// Simplified w/ map()
+const simplified = nums.map(function (num) {return num * 2});
+console.log(simplified)
+
+
+// Simplfied w/ map() + arrow function
+const simplified1 = nums.map(num => num * 2);
+console.log(simplified1)
+
+// With objects:
+const students = [
+    {
+      id: 1,
+      name: 'Mark',
+      profession: 'Developer',
+      skill: 'JavaScript'
+    },
+    {
+      id: 2,
+      name: 'Ariel',
+      profession: 'Developer',
+      skill: 'HTML'
+    },
+    {
+      id: 3,
+      name: 'Jason',
+      profession: 'Designer',
+      skill: 'CSS'
+    },
+  ];
+
+const student = students.map(student => [student.name, student.id]);
+console.log(student)
+
+const student1 = students.map(student => (
+    {
+        id: student.id,
+        name: student.name
+    }
+    ));
+console.log(student1)
